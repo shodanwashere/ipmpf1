e9314751774c092f3156c21c81043cdfbaf47c8a
Nuno Dias <jookaada@protonmail.com>
2022-04-14 19:44:05 +0100

    added stuff for temps

---
+++ js/temps_behavior.js
@@ -0,0 +1,19 @@
+function increaseTemp(id){
+    let ul = document.querySelector("#temps-menu-interior").childNodes;
+
+    let tempLabel = ul[id].getElementById("temp");
+    let temp = parseInt(tempLabel.textContent);
+    temp += 1;
+    tempLabel.textContent = temp.toString();
+}
+
+function decreaseTemp(id){
+    let ul = document.querySelector("#temps-menu-interior").childNodes;
+
+    let tempLabel = ul[id].getElementById("temp");
+    let temp = parseInt(tempLabel.textContent);
+    temp -= 1;
+    tempLabel.textContent = temp.toString();
+}
+
+// implement way to add event listeners to all the buttons
